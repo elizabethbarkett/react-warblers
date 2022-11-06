16a17832aa8d387030afb8bc1c590cb9686dde54
elizabethbarkett <ebarkett92@gmail.com>
2022-11-06 11:55:09 -0500

    moving function down into class

--- src/components/imageRow/index.js
+++ src/components/imageRow/index.js
@@ -5,24 +5,6 @@ import styles from './imageRow.module.css'
 const jsonData = require('../../birds.json');
 const loadData = () => JSON.parse(JSON.stringify(jsonData))
 
-function shuffle(array) {
-    //thanks stack overflow https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-    let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle.
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-    return array;
-  }
 
 export default class ImageRow extends Component {
     constructor(props) {
@@ -67,9 +49,29 @@ export default class ImageRow extends Component {
             }
         }
         console.log(birdArray)
-        let shuffledArray = shuffle(birdArray)
+        let shuffledArray = this.shuffle(birdArray)
         this.setState({birdsArray: shuffledArray})
     }
+
+    shuffle(array) {
+        //thanks stack overflow https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+        let currentIndex = array.length,  randomIndex;
+      
+        // While there remain elements to shuffle.
+        while (currentIndex != 0) {
+      
+          // Pick a remaining element.
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+      
+          // And swap it with the current element.
+          [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+        }
+      
+        return array;
+      }
+    
 
     render() {
         console.log(loadData())
